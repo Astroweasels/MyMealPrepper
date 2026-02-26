97e6a03baae10cfbac8f794f7b9dbffb520a45a4
Astroweasels <kole_duran@hotmail.com>
2026-02-26 11:55:38 -0600

    Set home as default

--- my-meal-prepper/src/app/app.routes.ts
+++ my-meal-prepper/src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [{
+export const routes: Routes = [
+ {
+    path: '',
+    redirectTo: 'body',
+    pathMatch: 'full'
+},    
+{
   path: 'about',
   loadComponent: () =>
     import('./components/about/about').then(m => m.About)
