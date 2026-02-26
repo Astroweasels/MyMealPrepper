1c7c3b710f49a598c2c68f3e65d1ee74ab36c79d
Astroweasels <kole_duran@hotmail.com>
2026-02-26 11:52:46 -0600

    Updated routes in main.ts

--- my-meal-prepper/src/app/app.ts
+++ my-meal-prepper/src/app/app.ts
@@ -1,13 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { Header } from './components/header/header';
-import { Body } from './components/body/body';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrls: ['./app.scss'],
   standalone: true,
-  imports: [Header, Body],
+  imports: [Header, RouterOutlet],
 })
 export class App {
   protected readonly title = signal('my-meal-prepper');
